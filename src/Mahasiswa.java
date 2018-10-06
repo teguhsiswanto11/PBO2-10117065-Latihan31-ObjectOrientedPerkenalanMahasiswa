949f87fade46e87288c7c4e17d579951d55747bf
Teguh Siswanto <tsiswanto73@gmail.com>
2018-10-06 09:33:39 +0700

    menghapus perintah get pada mahasiswa.java

--- src/Mahasiswa.java
+++ src/Mahasiswa.java
@@ -3,16 +3,9 @@ public class Mahasiswa {
     public String nim;
     public String nama;
 
-    public String getNama() {
-        return nama;
-    }
 
     public void setNama(String nama) {
         this.nama = nama;
-    }
-
-    public String getNim() {
-        return nim;
     }
 
     public void setNim(String nim) {
