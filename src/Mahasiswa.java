e3e89e158a4a5140ae9de9692931c75f38ed0158
Teguh Siswanto <tsiswanto73@gmail.com>
2018-10-06 20:57:22 +0700

    merapikan kode

--- src/Mahasiswa.java
+++ src/Mahasiswa.java
@@ -3,19 +3,10 @@ public class Mahasiswa {
     public String nim;
     public String nama;
 
-
-    public void setNama(String nama) {
-        this.nama = nama;
-    }
-
-    public void setNim(String nim) {
-        this.nim = nim;
-    }
-
-    public void perkenalkanDiri(){
+    public void perkenalkanDiri(String parNim, String parNama){
         System.out.println("Hallo Everyone");
-        System.out.println("My NIM is "+nim);
-        System.out.println("My Name is "+nama+"\n");
+        System.out.println("My NIM is "+parNim);
+        System.out.println("My Name is "+parNama+"\n");
     }
 
 
--- src/main.java
+++ src/main.java
@@ -12,24 +12,24 @@ public class main {
     public static void main(String[] args) {
 
         Mahasiswa mhs1 = new Mahasiswa();
-        mhs1.setNim("10110269");
-        mhs1.setNama("Rizki Adam Kurniawan");
-        mhs1.perkenalkanDiri();
+        mhs1.nim = "10110269";
+        mhs1.nama = "Rizki Adam Kurniawan";
+        mhs1.perkenalkanDiri(mhs1.nim,mhs1.nama);
 
         Mahasiswa mhs2 = new Mahasiswa();
-        mhs2.setNim("10110270");
-        mhs2.setNama("Indra Tiola");
-        mhs2.perkenalkanDiri();
+        mhs2.nim = "10110270";
+        mhs2.nama = "Indra Tiola";
+        mhs2.perkenalkanDiri(mhs2.nim,mhs2.nama);
 
         Mahasiswa mhs3 = new Mahasiswa();
-        mhs3.setNim("10110271");
-        mhs3.setNama("Robi Tanzil Ganefi");
-        mhs3.perkenalkanDiri();
+        mhs3.nim = "10110271";
+        mhs3.nama = "Robi Tanzil Ganefi";
+        mhs3.perkenalkanDiri(mhs3.nim,mhs3.nama);
 
         Mahasiswa mhs4 = new Mahasiswa();
-        mhs4.setNim("10110272");
-        mhs4.setNama("Muhammad Nur Awaluddin");
-        mhs4.perkenalkanDiri();
+        mhs4.nim = "10110272";
+        mhs4.nama = "Muhammad Nur Awaluddin";
+        mhs4.perkenalkanDiri(mhs4.nim,mhs4.nama);
 
     }
 
